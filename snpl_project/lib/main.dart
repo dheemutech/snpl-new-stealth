2553d0ac488a2a5981bc8dc60ebb8e65f1b25845
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2022-04-09 21:33:23 +0530

    Added Firebase

--- snpl_project/lib/main.dart
+++ snpl_project/lib/main.dart
@@ -2,10 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:snpl_project/screens/homepage.dart';
-void main() => runApp(const MyApp());                 
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
